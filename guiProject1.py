50d658dc7460651deda8ee30d07ad41af4de4cc1
moralesjoshua <151475871+moralesjoshua@users.noreply.github.com>
2023-11-28 17:37:25 -0600

    Start

--- guiProject1.py
+++ guiProject1.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk, Image
 
 
 class Gui:
@@ -7,14 +6,30 @@ class Gui:
         self.window = window
 
         # VOTE BUTTON
+        self.frame_one = Frame(self.window)
+        self.vote_now_button = Button(self.frame_one, text='VOTE NOW', command=self.show_voting_options)
+        self.vote_now_button.pack()
+        self.frame_one.pack(anchor='n', side="left", padx=10, pady=10)
+
+        # TOTAL VOTES BUTTON
         self.frame_two = Frame(self.window)
-        self.button_exit = Button(self.frame_two, text='VOTE NOW', command=window.quit)
-        self.button_exit.pack(pady=5)
+        self.total_votes_button = Button(self.frame_two, text='SHOW TOTAL VOTES', command=self.total_votes)
+        self.total_votes_button.pack()
+        self.frame_two.pack(anchor='n', side='left', padx=10, pady=10)
 
         # EXIT BUTTON
-        self.button_exit = Button(self.frame_two, text='EXIT', command=window.quit)
-        self.button_exit.pack()
-        self.frame_two.pack(anchor='s', side='bottom', pady=20)
+        self.frame_three = Frame(self.window)
+        self.exit_button = Button(self.frame_three, text='EXIT', command=window.quit)
+        self.exit_button.pack()
+        self.frame_three.pack(anchor='n', side='right', padx=10, pady=10)
+
+    def show_voting_options(self):
+        pass
+
+    def total_votes(self):
+        pass
+
+
 
 
 
