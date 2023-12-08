8352ef42ba91035964f76aa78b61d19fe6284bff
moralesjoshua <151475871+moralesjoshua@users.noreply.github.com>
2023-12-08 16:05:01 -0600

    Start

--- Project1.py
+++ Project1.py
@@ -3,10 +3,10 @@ from guiProject1 import *
 
 def main():
     window = Tk()
-    window.title('Cast your vote!')
-    window.geometry('500x500')
+    window.title('Voting')
+    window.geometry('400x375')
     window.resizable(False, False)
-    Gui(window)
+    VoteGui(window)
     window.mainloop()
 
 
